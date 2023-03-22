f62af035ea43ae0d2909eefde5b0e1479c9629dd
martin (macOS) <steam.martinjuan@gmail.com>
2023-03-22 13:06:08 +0800

    changed port

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ mongoose.connect(process.env.MONGO_URI)
   })
 
 
+
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:false}))
@@ -46,6 +47,6 @@ const generateUserID = () => {
 }
 
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(process.env.PORT || 3001, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
