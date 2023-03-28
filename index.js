658e60557837ec1da7290b4bda90797e531129a2
martinStudent <202010803@feualabang.edu.ph>
2023-03-28 17:38:50 +0800

    finished GET /api/users/:_id/logs route

--- index.js
+++ index.js
@@ -92,10 +92,10 @@ app.get('/api/users/:_id/logs', (req, res) => {
   let {limit} = req.query;
 
   if (from) {
-    from = from.trim() ? new Date(from.trim()) : new Date(0); // defaults to 1970-01-01 beginning of time
+    from = from.trim() ? new Date(from.trim()) : new Date("0"); // defaults to 1970-01-01 beginning of time
   }
   else {
-    from = new Date(0);
+    from = new Date("0");
   }
 
   if (to) {
@@ -119,76 +119,23 @@ app.get('/api/users/:_id/logs', (req, res) => {
   else if (to == 'Invalid Date') {
     res.status(400).json('Invalid date format for to parameter');
   }
-  else if (!(from <= to)) {
-    console.log(`from: ${from}, to: ${to}`)
-    res.status(400).json('Invalid date range');
-  }
 
-  console.log(from);
-  console.log(new Date(from.toISOString()));
+  userModel.aggregate([{ $match: { _id: userId }}, // match the document based on the user ID
+    { $unwind: '$exercises'}, // unwind the exercises array
+    { $match: {'exercises.date' : { $gte: from, $lte: to}} }, // filter exercises based on date range
+    { $limit: Number(limit) }, // limit the number of exercises
+    { $group: {_id: '$_id', username: {$first: '$username'}, exercises: {$push: '$exercises'}} }, // group exercises back into array by user
+  ])
+    .then(doc => {
+      doc = doc[0];
+      doc.count = doc.exercises.length;
 
-  userModel
-    .find({
-      _id: userId,
-      exercises: {
-        $elemMatch: {
-          date: {
-            $gte: new Date(from.toISOString()),
-            $lte: new Date(to.toISOString()),
-          }
-        }
-      }
+      res.json(doc);
     })
-    .limit(limit)
-    .select('_id username exercises')
-    .exec()
-    .then( user => {
-      var userObject = user[0].toObject();
-      userObject.exercises.filter( exercise => {
-        console.log(`exercise.date: ${exercise.date}, from: ${from}`);
-        return new Date(exercise.date.toISOString()) >= new Date(from.toISOString())
-      })
-      userObject.count = userObject.exercises.length;
-      res.json(userObject);
+    .catch(error => {
+      console.log(error);
+      res.status(500);
     })
-    .catch( err => {
-      res.status(500).send(err);
-    });
-
-
-  // const user = userModel.findById(userId);
-  // user.exec()
-  //   .then( user => {
-  //     if (user) {
-  //       // if user found, then return the user object
-  //       const userObject = {
-  //         _id: user.id,
-  //         username: user.username,
-  //         count: user.exercises.length,
-  //         log: user.exercises.filter( exercise => {
-  //           console.log(`exercise.date: ${exercise.date.toUTCString()} (${exercise.date.toUTCString() >= from.toUTCString()}), from: ${from.toUTCString()}, to: ${to.toUTCString()} (${exercise.date.toUTCString() <= to.toUTCString()})`);
-  //           return exercise.date.toUTCString() >= from.toUTCString() && exercise.date.toUTCString() <= to.toUTCString()
-  //         })
-  //           .map( exercise => {
-  //             return {
-  //               description: exercise.description,
-  //               duration: exercise.duration,
-  //               date: exercise.date.toDateString(),
-  //             }
-  //           })
-  //           .slice(0, limit)
-  //       }
-  //       res.json(userObject);
-  //     }
-  //     else {
-  //       // if user not found, then return 404
-  //       res.status(404).json('User not found');
-  //     }
-  //   })
-  //   .catch( error => {
-  //     console.log(error);
-  //     res.status(500);
-  //   })
 
 })
 
